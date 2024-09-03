b5952fff46c2e76e7874b6d38fba969043a2d153
muchiri08 <mbogokennedy08@gmail.com>
2024-09-03 22:33:51 +0300

    add create table appUser migrations

---
+++ src/main/resources/db/migration/V1.0.0__create_table_appUser.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS appUser(
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    passwordHash TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    role TEXT,
+    address TEXT,
+    activated BOOLEAN,
+    createdAt TIMESTAMP
+);
