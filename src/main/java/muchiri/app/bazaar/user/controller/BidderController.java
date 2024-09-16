129d00e7082126e33ba4e3ed17491945aa5e0b2e
kennedy <kennedy@mspace.co.ke>
2024-09-16 17:10:47 +0300

    add jakarta validation dep

--- src/main/java/muchiri/app/bazaar/user/controller/BidderController.java
+++ src/main/java/muchiri/app/bazaar/user/controller/BidderController.java
@@ -1,22 +1,32 @@
 package muchiri.app.bazaar.user.controller;
 
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
+import muchiri.app.bazaar.APIResponse;
 import muchiri.app.bazaar.user.model.Bidder;
 import muchiri.app.bazaar.user.service.UserService;
 
 @Path("bidders")
+@Produces(MediaType.APPLICATION_JSON)
 public class BidderController {
     @Inject
     UserService userService;
 
     @POST
     @Path("new")
-    public Response newBidder(Bidder bidder) {
+    public Response newBidder(@Valid Bidder bidder) {
         var newBidder = userService.newBidder(bidder);
-        return Response.status(Status.CREATED).entity(newBidder).build();
+        var status = Status.ACCEPTED.getStatusCode();
+        var response = new APIResponse<Bidder>(status, "success");
+
+        //TODO: Send Activation Email
+
+        return Response.status(status).entity(response).build();
     }
 }
--- src/main/java/muchiri/app/bazaar/user/model/User.java
+++ src/main/java/muchiri/app/bazaar/user/model/User.java
@@ -2,11 +2,18 @@ package muchiri.app.bazaar.user.model;
 
 import java.time.Instant;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public sealed class User permits Seller, Bidder {
     private long id;
+    @NotBlank(message = "name is required")
     private String name;
+    @Email(message = "email is invalid")
     private String email;
+    @NotBlank(message = "password is required")
     private String password;
+    @NotBlank(message = "phone is required")
     private String phone;
     private Role role;
     private String address;
