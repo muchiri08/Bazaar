e5dffa1d147a1b3f34932c1db1c04b4877a1b6b7
muchiri08 <mbogokennedy08@gmail.com>
2024-09-30 11:45:27 +0300

    add bid model

---
+++ src/main/java/muchiri/app/bazaar/bid/model/Bid.java
@@ -0,0 +1,62 @@
+package muchiri.app.bazaar.bid.model;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class Bid {
+    private long id;
+    private long bidderId;
+    private long productId;
+    private BigDecimal bidAmount;
+    private Instant updatedAt;
+    private short version;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getBidderId() {
+        return bidderId;
+    }
+
+    public void setBidderId(long bidderId) {
+        this.bidderId = bidderId;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getBidAmount() {
+        return bidAmount;
+    }
+
+    public void setBidAmount(BigDecimal bidAmount) {
+        this.bidAmount = bidAmount;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public short getVersion() {
+        return version;
+    }
+
+    public void setVersion(short version) {
+        this.version = version;
+    }
+
+}
